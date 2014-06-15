c84614780c12fb584927e45b349fb68e258f20c7
rfaulk <rfaulkner@wikimedia.org>
2014-06-15 14:14:41 -0700

    add - implementation for redis communication

--- src/redis.h
+++ src/redis.h
@@ -10,21 +10,60 @@
 
 #include <iostream>
 #include <string>
+#include <redis3m/redis3m.hpp>
 
 using namespace std;
 
+
+/**
+ *  Defines interface to redis server
+ */
 class RedisHandler {
     string host;
     string database;
+    redis3m::connection::ptr_t conn;
+    
 public:
     RedisHandler() {}
     RedisHandler(string, string);
+    
     bool connect();
-    bool write(string key, int value);
-    int read(string key);
+    bool write(string, string);
+    string read(string);
 };
 
+
+/**
+ *  Constructor that allows specification of host and db
+ */
 RedisHandler::RedisHandler(string host, string database) {
     this->host = host;
     this->database = database;
+}
+
+
+/**
+ *  Establishes a connection to a redis instance
+ */
+bool RedisHandler::connect() {
+    redis3m::connection::ptr_t conn = redis3m::connection::create();
+    return true;
+}
+
+
+/**
+ *  Writes a key value to redis
+ */
+bool RedisHandler::write(string key, string value) {
+    redis3m::conn->run(redis3m::command("SET") << key << value );
+    return true;
+}
+
+
+/**
+ *  Read a value from redis given a key
+ */
+string RedisHandler::read(string key) {
+    redis3m::reply r = conn->run(command("GET") << key );
+    return reply;
 }
