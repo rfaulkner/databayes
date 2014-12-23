5cdda01e76ea649913c381a42796fedcea63f4d7
rfaulk <rfaulk@yahoo-inc.com>
2014-12-23 09:40:51 -0800

    add some confgig

---
+++ http/databayes-api/config.py
@@ -0,0 +1,13 @@
+
+__version__ = '0.0.1'
+
+LICENSE = "BSD (Three Clause)"
+AUTHORS = { 'Ryan Faulkner': 'bobs.ur.uncle@gmail.com'}
+ADMINS = { 'Ryan Faulkner': 'bobs.ur.uncle@gmail.com' }
+FLASK_LOG = '/var/log/flask.log'
+SITE_URL = 'http://databayes-api.com'
+
+__instance_host__ = '127.0.0.1'
+__instance_host_vagrant__ = '0.0.0.0'
+__instance_port__ = 5000
+
