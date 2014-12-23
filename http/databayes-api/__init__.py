c7838d0be9325cbb8e13b9cd47c618ffedf71778
rfaulk <rfaulk@yahoo-inc.com>
2014-12-23 09:42:59 -0800

    some more module level stuff

--- http/databayes-api/__init__.py
+++ http/databayes-api/__init__.py
@@ -3,6 +3,16 @@ __author__ = 'rfaulk'
 from databayes_api import config
 from flask import Flask
 from flask.ext.login import LoginManager
+import logging
+
+FORMAT="%(asctime)s %(levelname)-8s %(message)s"
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger(__name__)
+log.setLevel(logging.ERROR)
+
+def log_set_level(level):
+    """ Set logging level """
+    log.setLevel(level)
 
 
 # Instantiate flask app
@@ -12,5 +22,9 @@ app.config['VERSION'] = config.__version__
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+
+
 
 
---
+++ http/databayes-api/views.py
@@ -0,0 +1,4 @@
+
+
+def init_views():
+    pass
