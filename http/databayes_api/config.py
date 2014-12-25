a271ddd8060cbfd13c76925e2edd3ce7672193ff
rfaulk <rfaulk@yahoo-inc.com>
2014-12-24 21:13:17 -0800

    ensure that a secret key exists

--- http/databayes_api/config.py
+++ http/databayes_api/config.py
@@ -7,6 +7,8 @@ ADMINS = { 'Ryan Faulkner': 'bobs.ur.uncle@gmail.com' }
 FLASK_LOG = '/var/log/flask.log'
 SITE_URL = 'http://databayes-api.com'
 
+__secret_key__ = "something"
+
 __instance_host__ = '127.0.0.1'
 __instance_host_vagrant__ = '0.0.0.0'
 __instance_port__ = 5000
