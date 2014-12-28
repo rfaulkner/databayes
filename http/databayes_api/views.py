732373ff808dcb207d04c8f331c3f6f70def33c8
rfaulk <rfaulk@yahoo-inc.com>
2014-12-28 13:22:04 -0800

    implement translation for "DEF"

--- http/databayes_api/views.py
+++ http/databayes_api/views.py
@@ -5,45 +5,58 @@
     layer handles communication to the databayes daemon.
 """
 
-from databayes_api import app, log, redisio, get_next_command
+from databayes_api import app, log, redisio, get_next_command, config, \
+    gen_queue_id
 import json
 
 from flask import render_template, redirect, url_for, \
     request, escape, flash, g, session, Response
 
 
-def define_entity():
+def define_entity(entity):
     """ Handles remote requests to databayes for entity definition
     :return:    JSON response indicating status of action & output
     """
     redisio.DataIORedis().connect()
-    cmd = redisio.DataIORedis().read("command_queue_" + get_next_command())
+    fields = request.args.get('fields').split(',')
+    types = request.args.get('types').split(',')
+
+    if len(fields) != len(types):
+        return Response(json.dumps(['number of fields and types do not match']),
+                        mimetype='application/json')
+
+    args = []
+    for i in xrange(len(fields)):
+        args[i] = fields[i] + '=' + types[i]
+    cmd = 'def {0}({1})'.format(entity, ",".join(args))
     # Send cmd to databayes daemon
-    return Response(json.dumps(['']),  mimetype='application/json')
+    redisio.DataIORedis().write(config.DBY_CMD_QUEUE_PREFIX + gen_queue_id(), cmd)
+
+    return Response(json.dumps(['Command Inserted']),  mimetype='application/json')
 
 def add_relation():
     """ Handles remote requests to databayes for adding relations
     :return:    JSON response indicating status of action & output
     """
-    return Response(json.dumps(['']),  mimetype='application/json')
+    return Response(json.dumps(['Command Inserted']),  mimetype='application/json')
 
 def generate():
     """ Handles remote requests to databayes for generating samples
     :return:    JSON response indicating status of action & output
     """
-    return Response(json.dumps(['']),  mimetype='application/json')
+    return Response(json.dumps(['Command Inserted']),  mimetype='application/json')
 
 def list_entity():
     """ Handles remote requests to databayes for listing entities
     :return:    JSON response indicating status of action & output
     """
-    return Response(json.dumps(['']),  mimetype='application/json')
+    return Response(json.dumps(['Command Inserted']),  mimetype='application/json')
 
 def list_relation():
     """ Handles remote requests to databayes for listing relations
     :return:    JSON response indicating status of action & output
     """
-    return Response(json.dumps(['']),  mimetype='application/json')
+    return Response(json.dumps(['Command Inserted']),  mimetype='application/json')
 
 # Stores view references in structure
 view_list = {
