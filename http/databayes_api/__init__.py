37b705b9496671c9c1375a2cb54af068002fa5da
rfaulk <rfaulk@yahoo-inc.com>
2014-12-28 14:00:48 -0800

    mehtod to determine which id the current queue item will be assigned

--- http/databayes_api/__init__.py
+++ http/databayes_api/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'rfaulk'
 
-from databayes_api import config
+from databayes_api import config, redisio
 from flask import Flask
 import logging
 
@@ -18,6 +18,24 @@ def log_set_level(level):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.__secret_key__
 app.config['VERSION'] = config.__version__
+
+def gen_queue_id():
+    """ Handles the logic to determine the ID for a new queue item
+    :return: the integer queue id
+    """
+    redisio.DataIORedis().connect()
+    counter_curr = redisio.DataIORedis().read(config.REDIS_QUEUE_COUNTER_KEY)
+    if int(counter_curr) < config.REDIS_QUEUE_COUNTER_MAX:
+        qid = counter_curr
+        counter_curr = int(counter_curr) + 1
+    else:
+        qid = config.REDIS_QUEUE_COUNTER_MAX
+        counter_curr = 0
+    redisio.DataIORedis().write(config.DBY_CMD_QUEUE_PREFIX + gen_queue_id(), counter_curr)
+    return qid
+
+
+
 
 
 
