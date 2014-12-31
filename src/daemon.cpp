9751ea2c4e17063dcbbd6c252eb0e4539ba94619
rfaulk <rfaulk@yahoo-inc.com>
2014-12-31 12:14:21 -0800

    implementation for getKeyOrderValue

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -38,10 +38,15 @@ std::string getNextQueueKey(RedisHandler& rh) {
 }
 
 /**
- * This method handles extracting the value from the redis command key
+ * This method handles extracting the value from the redis command key.  The parser
+ * functionality to tokenize strings is used to assist
  */
-long getKeyOrderValue(std::string key) {
-    return 1;
+long getKeyOrderValue(Parser& p, std::string key) {
+    vector<std::string> pieces = p.tokenize(key);
+    if (pieces.length() > 0)
+        return pieces[pieces.length() - 1];
+    else
+        return -1;  // error
 }
 
 int main() {
@@ -73,7 +78,8 @@ int main() {
         }
 
         // 3. Parse the command and write response to redis
-        redisHandler->write(std::string(DBY_RSP_QUEUE_PREFIX) + getKeyOrderValue(key), parser->parse(line));
+        long key_value = getKeyOrderValue(key);
+        redisHandler->write(std::string(DBY_RSP_QUEUE_PREFIX) + key_value, parser->parse(line));
         parser->resetState();
 
         // 4. Remove the element and it's lock
