ca9851792c17d8a2da14574722fa0cf4a7e26ec9
rfaulk <rfaulkner@wikimedia.org>
2015-03-31 01:48:02 +0100

    refactor interface of cli emit methods to exclude flag indicating debug mode.  This behaviour will be handled by calling code.

--- src/emit.h
+++ src/emit.h
@@ -11,10 +11,10 @@
 #ifndef _emit_h
 #define _emit_h
 
-void emitCLIError(std::string message, bool isDebug = false) { cout << std::string("ERR: ") + message << endl; }
+void emitCLIError(std::string message) { cout << std::string("ERR: ") + message << endl; }
 
-void emitCLIWarning(std::string, bool isDebug = false) { cout << std::string("WARN: ") + message << endl; }
+void emitCLIWarning(std::string) { cout << std::string("WARN: ") + message << endl; }
 
-void emitCLINote(std::string, bool isDebug = false) { cout << std::string("NOTE: ") + message << endl; }
+void emitCLINote(std::string) { cout << std::string("NOTE: ") + message << endl; }
 
 #endif
