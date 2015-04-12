654e0d6e521cb25cff147b1069c84c848c5adbc6
Ryan Faulkner <bobs.ur.uncle@gmail.com>
2015-04-12 13:29:32 +0100

    local user for vagrant key location

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to deploy to vagrant dev instance
-rsync -azPr -e "ssh -p 2222 -i /Users/rfaulkner/.vagrant.d/insecure_private_key" . vagrant@127.0.0.1:/home/vagrant/databayes > push.log
+rsync -azPr -e "ssh -p 2222 -i /Users/$USER/.vagrant.d/insecure_private_key" . vagrant@127.0.0.1:/home/vagrant/databayes > push.log
