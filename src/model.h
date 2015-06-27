00940f601166a9215ca8819e084fca7e72f77228
Ryan Faulkner <bobs.ur.uncle@gmail.com>
2015-06-27 20:08:47 +0100

    fixes to Relation::write

--- src/model.h
+++ src/model.h
@@ -379,22 +379,22 @@ public:
 
     void write(RedisHandler& rds) {
 
-    /* TODO implement */
-
+        Json::Value jsonVal = this->toJson();
         std::string key;
-        rds->connect();
-        key = this->generateRelationKey(std::string(jsonVal[JSON_ATTR_REL_ENTL].asCString()), std::string(jsonVal[JSON_ATTR_REL_ENTR].asCString()), generateRelationHash(jsonVal));
+        rds.connect();
+        key = this->generateKey();
 
-        if (this->redisHandler->exists(key)) {
+        if (rds.exists(key)) {
             if (this->fetchRaw(key, jsonVal)) {
-                jsonVal[JSON_ATTR_REL_COUNT] = jsonVal[JSON_ATTR_REL_COUNT].asInt() + 1;
+                jsonVal[JSON_ATTR_REL_COUNT] =
+                    jsonVal[JSON_ATTR_REL_COUNT].asInt() + 1;
             } else
                 return false;
         } else
             jsonVal[JSON_ATTR_REL_COUNT] = 1;
 
-        rds->incrementKey(KEY_TOTAL_RELATIONS, 1);
-        rds->write(key, jsonVal.toStyledString());
+        rds.incrementKey(KEY_TOTAL_RELATIONS, 1);
+        rds.write(key, jsonVal.toStyledString());
         return true;
     }
 
